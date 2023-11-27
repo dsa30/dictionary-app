2332221c206c62e237e9047ac265d2f64b9795f7
Danie Selase Anyidoho <adanielselase@gmail.com>
2023-11-27 00:28:50 -0800

    add html content and fetch

--- main.js
+++ main.js
@@ -1,24 +1,24 @@
-import './style.css'
-import javascriptLogo from './javascript.svg'
-import viteLogo from '/vite.svg'
-import { setupCounter } from './counter.js'
+const form = document.getElementById("form");
+const input = document.getElementById("get-input");
 
-document.querySelector('#app').innerHTML = `
-  <div>
-    <a href="https://vitejs.dev" target="_blank">
-      <img src="${viteLogo}" class="logo" alt="Vite logo" />
-    </a>
-    <a href="https://developer.mozilla.org/en-US/docs/Web/JavaScript" target="_blank">
-      <img src="${javascriptLogo}" class="logo vanilla" alt="JavaScript logo" />
-    </a>
-    <h1>Hello Vite!</h1>
-    <div class="card">
-      <button id="counter" type="button"></button>
-    </div>
-    <p class="read-the-docs">
-      Click on the Vite logo to learn more
-    </p>
-  </div>
-`
+const getUserInput = (event) => {
+  const userInput = input.value;
+  console.log(userInput);
+  getWordInfo(userInput);
+  event.preventDefault();
+};
 
-setupCounter(document.querySelector('#counter'))
+form.addEventListener("submit", getUserInput);
+
+async function getWordInfo(word) {
+  try {
+    const response = await fetch(
+      `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
+    );
+    const data = await response.json();
+    // const dictionaryInfo = { word, phonetics };
+    console.log(data);
+  } catch (error) {
+    console.log(error);
+  }
+}
