a55612e1fe9560a75f46717deb59924bc0a10d8f
Danie Selase Anyidoho <adanielselase@gmail.com>
2023-11-27 08:40:07 -0800

    add word example

--- main.js
+++ main.js
@@ -1,5 +1,8 @@
 const form = document.getElementById("form");
 const input = document.getElementById("get-input");
+const wordDefinition = document.getElementById("word");
+const wordPhonetic = document.getElementById("phonetic");
+const sourceUrl = document.getElementById("urls");
 
 const getUserInput = (event) => {
   const userInput = input.value;
@@ -16,9 +19,49 @@ async function getWordInfo(word) {
       `https://api.dictionaryapi.dev/api/v2/entries/en/${word}`
     );
     const data = await response.json();
-    // const dictionaryInfo = { word, phonetics };
-    console.log(data);
+    const dictionaryInfo = {
+      word: data[0].word,
+      phonetic: data[0].phonetic,
+      definitions: data[0].meanings[0].definitions,
+      definitionsVerb: data[0].meanings[1].definitions,
+      urls: data[0].sourceUrls[0],
+    };
+    displayWordInfo(dictionaryInfo);
+    getDefinitions(dictionaryInfo);
   } catch (error) {
     console.log(error);
   }
 }
+
+const displayWordInfo = (displayWordObj) => {
+  console.log(displayWordObj);
+  const { word, phonetic, urls, definitions, definitionsVerb } = displayWordObj;
+  wordDefinition.innerText = word;
+  wordPhonetic.innerText = phonetic === undefined ? "N/A" : phonetic;
+  sourceUrl.innerText = urls;
+};
+
+const getDefinitions = (displayWordObj) => {
+  const { definitions, definitionsVerb } = displayWordObj;
+  let nounDefinition = document.getElementById("noun-definition");
+  let verbDefinition = document.getElementById("verb-definitions");
+  let example = document.getElementById("example");
+  let meaningList = "";
+  let meaningListVerb = "";
+  let meaningExample = "";
+  for (const definition in definitions) {
+    meaningList += `<li>${definitions[definition].definition}</li>`;
+  }
+
+  for (const vDefinition in definitionsVerb) {
+    meaningListVerb += `<li>${definitionsVerb[vDefinition].definition}</li>`;
+    let wordExample = definitionsVerb[vDefinition].example;
+    if (wordExample) {
+      meaningExample += `<li>${definitionsVerb[vDefinition].example}</li>`;
+    }
+  }
+
+  nounDefinition.innerHTML = meaningList;
+  verbDefinition.innerHTML = meaningListVerb;
+  example.innerHTML = meaningExample;
+};
